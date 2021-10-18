56d50219363e9fa4f412720a1396b286d14c6a67
raviiraj60 <71936788+raviiraj60@users.noreply.github.com>
2021-10-18 08:41:56 +0530

    Create RaviRaj.js

---
+++ RaviRaj.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>JavaScript Hello World Example</title>
+    <script>
+        alert('Hello, World!');
+    </script>
+</head>
+<body>
+</body>
+</html>
